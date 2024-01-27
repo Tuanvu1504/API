3b8607bbaf16725760d0a246b6241e5a9ac98054
Tuanvu1504 <vutuanseducs4@gmail.com>
2024-01-27 23:23:19 +0700

    pwe

--- src/index.js
+++ src/index.js
@@ -12,14 +12,12 @@ const app = express();
 app.use(express.json())
 app.use(cors())
 
-mongoose.connect(DB_URL ).then(()=>{
+mongoose.connect(DB_URL).then(()=>{
   console.log('connection established')
 })
 
 app.use ('/api',router )
-app.get('/', (req,res)=>{
-  res.send('hey this is my api running')
-})
+
 
 
 app.listen(PORT, ()=>{
